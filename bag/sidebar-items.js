31d3a39d2578810b87e3238e641c5e5b39b10328
Steven Allen <steven@stebalien.com>
2016-06-30 16:29:06 -0400

    Update docs for b029068298937fb35e39de9cb6f2f2fa1f150879
    Fix documentation url

--- bag/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"struct":[["Bag","A data structure storing values indexed by tickets."],["Indices",""],["IntoIter",""],["Iter",""],["IterMut",""]]});
--- implementors/core/cmp/trait.PartialEq.js
+++ implementors/core/cmp/trait.PartialEq.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['snowflake'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='struct' href='snowflake/struct.ProcessUniqueId.html' title='snowflake::ProcessUniqueId'>ProcessUniqueId</a>",];implementors["stash"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='struct' href='stash/unique_stash/struct.Tag.html' title='stash::unique_stash::Tag'>Tag</a>",];implementors["stash"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='struct' href='stash/unique_stash/struct.Tag.html' title='stash::unique_stash::Tag'>Tag</a>",];
+implementors["stash"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='struct' href='stash/unique_stash/struct.Tag.html' title='stash::unique_stash::Tag'>Tag</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
