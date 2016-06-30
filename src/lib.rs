076280307822651434fab489181cdefe93f49340
Steven Allen <steven@stebalien.com>
2016-06-30 17:48:50 -0400

    Talk about how fast stash is

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 //! Stash is a library for efficiently storing maps of keys to values when one
-//! doesn't care what the keys are but wants blazing fast `O(1)` insertions,
-//! deletions, and lookups.
+//! doesn't care what the keys are but wants blazing<sup>†</sup> fast `O(1)`
+//! insertions, deletions, and lookups.
 //!
 //! Common use cases include file descriptor tables, session tables, or MIO
 //! context tables.
+//!
+//! <sup>†</sup>Blazing means an order of magnitude faster than hash maps and btree maps.
 
 #[macro_use]
 mod iter_macro;
