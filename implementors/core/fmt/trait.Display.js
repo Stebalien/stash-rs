716db0f0c743931fceb7a46a38c311cecc3fc255
Steven Allen <steven@stebalien.com>
2016-08-27 16:30:35 -0400

    Update docs for eb1d59df7c2730381435651f01235af7923419b5
    Add method for parsing a tag string into a Tag

--- implementors/core/fmt/trait.Display.js
+++ implementors/core/fmt/trait.Display.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["stash"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='struct' href='stash/unique_stash/struct.Tag.html' title='stash::unique_stash::Tag'>Tag</a>",];
+implementors["stash"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='struct' href='stash/unique_stash/struct.TagParseError.html' title='stash::unique_stash::TagParseError'>TagParseError</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='struct' href='stash/unique_stash/struct.Tag.html' title='stash::unique_stash::Tag'>Tag</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
