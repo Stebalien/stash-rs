cb51ce5f6bf52ae38233fb81c767bc4024d5f093
Steven Allen <steven@stebalien.com>
2017-01-02 19:22:41 -0800

    document guarantees

--- src/stash/mod.rs
+++ src/stash/mod.rs
@@ -101,6 +101,19 @@ impl_iter!(IntoIter, (<V>), (usize, V), entry::value_index);
 
 /// An `O(1)` amortized table that reuses keys.
 ///
+/// # Guarantees and non-guarantees:
+///
+/// 1. `Stash` is deterministic and keys do not depend on the inserted values.
+///    This means you can update two stashes in tandem and get the same keys
+///    back. This could be useful for, e.g., primary/secondary replication.
+/// 2. Keys will always be less than the maximum number of items that have ever
+///    been present in the `Stash` at any single point in time. In other words,
+///    if you never store more than `n` items in a `Stash`, the stash will only
+///    assign keys less than `n`. You can take advantage of this guarantee to
+///    truncate the key from a `usize` to some smaller type.
+/// 3. Except the guarantees noted above, you can assume nothing about key
+///    assignment or iteration order. They can change at any time.
+///
 /// An example use case is a file descriptor table.
 #[derive(Clone)]
 pub struct Stash<V> {
