6448683d8d44804068a7c5d17d42da47b3d85e79
Shane Pearman <spearman@github.com>
2017-01-06 21:16:26 -0800

    document clear() behavior

--- src/stash/mod.rs
+++ src/stash/mod.rs
@@ -386,7 +386,8 @@ impl<V> Stash<V> {
         }
     }
 
-    /// Clear the stash
+    /// Clear the stash. Cleared stash will give the same keys as a
+    /// new stash for subsequent puts.
     pub fn clear(&mut self) {
         // Do it this way so that nothing bad happens if a destructor panics.
         for (i, entry) in self.data.iter_mut().enumerate() {
